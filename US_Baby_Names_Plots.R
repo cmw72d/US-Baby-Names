8cd9cbff0a747eda9d1952b9918aeb8cc000babb
ampwd6 <ampwd6@mail.missouri.edu>
2016-04-10 16:38:16 -0500

    updated r file again

--- US_Baby_Names_Plots.R
+++ US_Baby_Names_Plots.R
@@ -7,6 +7,24 @@ filtered_extrema = read.csv("filtered_extrema.csv")
 female = filtered_extrema %>%
   filter(Gender == 'F', ROC > 0, Count >= 500)
 ggplot(female, aes(x = Year,y = ROC,col = Name))+geom_point()
+
 male = filtered_extrema %>%
   filter(Gender == 'M', ROC > 0, Count >= 500)
 ggplot(male, aes(x = Year,y = ROC,col = Name))+geom_point()
+
+both = filtered_extrema %>%
+  filter(ROC > 0, Count >= 500)
+ggplot(both, aes(x = Year,y = ROC,col = Name))+geom_point()
+
+female_neg = filtered_extrema %>%
+  filter(Gender == 'F', ROC < 0, Count >= 500)
+ggplot(female_neg, aes(x = Year,y = ROC,col = Name))+geom_point()
+
+male_neg = filtered_extrema %>%
+  filter(Gender == 'M', ROC < 0, Count >= 500)
+ggplot(male_neg, aes(x = Year,y = ROC,col = Name))+geom_point()
+
+both_neg = filtered_extrema %>%
+  filter(ROC < 0, Count > 100)
+ggplot(both_neg, aes(x = Year,y = ROC,col = Name))+geom_point()
+
