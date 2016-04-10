0a1c5f2dcfb98528a607a561c225d4ddac537abc
Will Starms <vilhelmen@gmail.com>
2016-04-09 21:16:28 -0500

    Export of the notebook. First hunk does the ROC, second finds the extrema. Seems flawed though :(

--- Baby_calc.r
+++ Baby_calc.r
@@ -16,10 +16,13 @@ parallel_roc = function(name_frame) {
 
     sub_list = parRapply(clus,pairs,function(x) {
         sub = subset(name_frame,Name==x[1] & Gender == x[2])
+        # name_frame[name_frame$Id %in% sub$Id,]["ROC"] <<- ROC(sub$Count)
         sub$ROC = ROC(sub$Count)
         return (sub)
     })
     stopCluster(clus)
+    
+    #name_frame = rbind.fill.parallel(sub_list)
     
     return (rbind_all(sub_list))
 }
@@ -27,3 +30,33 @@ Sys.time()
 roc_names = parallel_roc(names)
 Sys.time()
 write.csv(roc_names,file="roc_data.csv",row.names=TRUE)
+
+
+require("TTR")
+require("parallel")
+require("dplyr")
+
+
+roc_data = read.csv(file = "~/roc_data.csv",header = TRUE)
+pairs = unique(roc_data[c("Year","Gender")]) # easy way to do something for every year/gender
+roc_data$ROC[which(is.na(roc_data$ROC))] = 0; # Just go away please
+
+
+
+clus = makeCluster(45)
+
+clusterExport(cl=clus,varlist=list("pairs","roc_data"),envir=environment())
+
+results = parRapply(clus,pairs,function(x){    
+    sub = subset(roc_data,Year==x[1] & Gender == x[2])
+    return (rbind(sub[which(sub$ROC == max(sub$ROC)),],sub[which(sub$ROC == min(sub$ROC)),]))
+})
+stopCluster(clus)
+
+
+
+results = rbind_all(results)
+
+write.csv(results,file="~/extrema.csv",row.names=TRUE)
+
+
